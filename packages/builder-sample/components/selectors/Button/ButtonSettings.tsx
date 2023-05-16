5c7180a6cb3dfb3756b423d14567b443deb26e76
Vedant Pandey <vedantpandey46@gmail.com>
2023-05-16 19:38:50 +0530

    Add Accordion component

--- packages/builder-sample/components/selectors/Button/ButtonSettings.tsx
+++ packages/builder-sample/components/selectors/Button/ButtonSettings.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
 
 import { ToolbarSection, ToolbarItem } from '../../editor';
+import { FLAccordion } from '@fralo-tech/components';
 
 export const ButtonSettings = () => {
   return (
     <React.Fragment>
+      <FLAccordion
+        summary={({ background, color }: any) => {
+          return (
+            <div className="flex flex-row-reverse">
+              <div
+                style={{
+                  background:
+                    background && `rgba(${Object.values(background)})`,
+                }}
+                className="shadow-md flex-end w-6 h-6 text-center flex items-center rounded-full bg-black"
+              >
+                <p
+                  style={{
+                    color: color && `rgba(${Object.values(color)})`,
+                  }}
+                  className="text-white w-full text-center"
+                >
+                  T
+                </p>
+              </div>
+            </div>
+          );
+        }}
+      >
+        <ToolbarItem
+          full={true}
+          propKey="background"
+          type="bg"
+          label="Background"
+        />
+        <ToolbarItem full={true} propKey="color" type="color" label="Text" />
+      </FLAccordion>
       <ToolbarSection
         title="Colors"
         props={['background', 'color']}
--- packages/components/src/lib/primitives/FLAccordion/index.tsx
+++ packages/components/src/lib/primitives/FLAccordion/index.tsx
@@ -1,14 +1,133 @@
 import styles from './index.module.css';
+import React from 'react';
+import classNames from 'classnames';
+import { RxChevronDown } from 'react-icons/rx';
+import * as Accordion from '@radix-ui/react-accordion';
+import { Divider, Grid } from '@material-ui/core';
+import { useNode } from '@fralo-tech/builder-core';
 
-/* eslint-disable-next-line */
-export interface FLAccordionProps {}
-
-export function FLAccordion(props: FLAccordionProps) {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to FLAccordion!</h1>
-    </div>
-  );
+export interface FLAccordionProps {
+  summary: unknown;
+  children?: JSX.Element | JSX.Element[] | ((e: unknown) => JSX.Element);
+  title?: string;
+  props: unknown;
 }
+
+export const FLAccordion = ({
+  title,
+  props,
+  summary,
+  children,
+}: FLAccordionProps) => {
+  const { nodeProps } = useNode((node) => ({
+    nodeProps:
+      props &&
+      props.reduce((res: any, key: any) => {
+        res[key] = node.data.props[key] || null;
+        return res;
+      }, {}),
+  }));
+  return (
+    <Accordion.Root
+      className="bg-mauve6 w-[300px] rounded-md shadow-[0_2px_10px] shadow-black/5"
+      type="single"
+      defaultValue="item-1"
+      collapsible
+    >
+      <Accordion.Item
+        value="item-1"
+        className="focus-within:shadow-mauve12 mt-px overflow-hidden first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px]"
+      >
+        <Accordion.Trigger className="text-violet11 shadow-mauve6 hover:bg-mauve2 group flex h-[45px] flex-1 cursor-default items-center justify-between bg-white px-5 text-[15px] leading-none shadow-[0_1px_0] outline-none">
+          <div className="px-6 w-full">
+            <Grid container direction="row" alignItems="center" spacing={3}>
+              <Grid item xs={4}>
+                <h5 className="text-sm text-light-gray-1 text-left font-medium text-dark-gray">
+                  {title}
+                </h5>
+              </Grid>
+              {summary && props ? (
+                <Grid item xs={8}>
+                  <h5 className="text-light-gray-2 text-sm text-right text-dark-blue">
+                    {summary(
+                      props.reduce((acc: any, key: any) => {
+                        acc[key] = nodeProps[key];
+                        return acc;
+                      }, {})
+                    )}
+                  </h5>
+                </Grid>
+              ) : null}
+            </Grid>
+          </div>
+          <RxChevronDown
+            className="text-violet10 ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180"
+            aria-hidden
+          />
+        </Accordion.Trigger>
+        <Accordion.Content className="text-mauve11 bg-mauve2 data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp overflow-hidden text-[15px]">
+          <Divider />
+          <Grid container spacing={1}>
+            {children}
+          </Grid>
+        </Accordion.Content>
+      </Accordion.Item>
+    </Accordion.Root>
+  );
+};
+
+const AccordionItem = React.forwardRef(
+  ({ children, className, ...props }, forwardedRef) => (
+    <Accordion.Item
+      className={classNames(
+        'focus-within:shadow-mauve12 mt-px overflow-hidden first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px]',
+        className
+      )}
+      {...props}
+      ref={forwardedRef}
+    >
+      {children}
+    </Accordion.Item>
+  )
+);
+
+const AccordionTrigger = React.forwardRef(
+  ({ children, className, ...props }, forwardedRef) => (
+    <Accordion.Header className="flex">
+      <Accordion.Trigger
+        className={classNames(
+          'text-violet11 shadow-mauve6 hover:bg-mauve2 group flex h-[45px] flex-1 cursor-default items-center justify-between bg-white px-5 text-[15px] leading-none shadow-[0_1px_0] outline-none',
+          className
+        )}
+        {...props}
+        ref={forwardedRef}
+      >
+        {children}
+        <RxChevronDown
+          className="text-violet10 ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180"
+          aria-hidden
+        />
+      </Accordion.Trigger>
+    </Accordion.Header>
+  )
+);
+
+const AccordionContent = React.forwardRef(
+  ({ children, className, ...props }, forwardedRef) => (
+    <Accordion.Content
+      className={classNames(
+        'text-mauve11 bg-mauve2 data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp overflow-hidden text-[15px]',
+        className
+      )}
+      {...props}
+      ref={forwardedRef}
+    >
+      {children}
+      {/* <div className="py-[15px] px-5">
+        {children}
+        </div> */}
+    </Accordion.Content>
+  )
+);
 
 export default FLAccordion;
