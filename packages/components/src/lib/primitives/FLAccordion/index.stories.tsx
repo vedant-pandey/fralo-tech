008e44287d25a190d23955a8706cbf709dd9f677
Vedant Pandey <vedantpandey46@gmail.com>
2023-05-16 19:40:01 +0530

    Create accordion story

---
+++ packages/components/src/lib/primitives/FLAccordion/index.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { FLAccordion } from './index';
+
+const Story: Meta<typeof FLAccordion> = {
+  component: FLAccordion,
+  title: 'FLAccordion',
+};
+export default Story;
+
+export const Primary = {
+  args: {},
+};
