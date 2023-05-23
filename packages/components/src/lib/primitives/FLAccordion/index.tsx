10cc7f99e0e49eeeff647d7bf7b1418c8a9687c7
Vedant Pandey <vedantpandey46@gmail.com>
2023-05-24 01:34:22 +0530

    Create multi accordion component

--- packages/components/src/lib/primitives/FLAccordion/index.tsx
+++ packages/components/src/lib/primitives/FLAccordion/index.tsx
@@ -7,10 +7,10 @@ import { Divider, Grid } from '@material-ui/core';
 import { useNode } from '@fralo-tech/builder-core';
 
 export interface FLAccordionProps {
-  summary: unknown;
+  summary?: unknown;
   children?: JSX.Element | JSX.Element[] | ((e: unknown) => JSX.Element);
   title?: string;
-  props: unknown;
+  props?: unknown;
 }
 
 export const FLAccordion = ({
@@ -38,7 +38,7 @@ export const FLAccordion = ({
         value="item-1"
         className="focus-within:shadow-mauve12 mt-px overflow-hidden first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px]"
       >
-        <Accordion.Trigger className="text-violet11 shadow-mauve6 hover:bg-mauve2 group flex h-[45px] flex-1 cursor-default items-center justify-between bg-white px-5 text-[15px] leading-none shadow-[0_1px_0] outline-none">
+        <Accordion.Trigger className="w-full text-violet11 shadow-mauve6 hover:bg-mauve2 group flex h-[45px] flex-1 cursor-default items-center justify-between bg-white px-5 text-[15px] leading-none shadow-[0_1px_0] outline-none">
           <div className="px-6 w-full">
             <Grid container direction="row" alignItems="center" spacing={3}>
               <Grid item xs={4}>
@@ -75,59 +75,5 @@ export const FLAccordion = ({
     </Accordion.Root>
   );
 };
-
-const AccordionItem = React.forwardRef(
-  ({ children, className, ...props }, forwardedRef) => (
-    <Accordion.Item
-      className={classNames(
-        'focus-within:shadow-mauve12 mt-px overflow-hidden first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px]',
-        className
-      )}
-      {...props}
-      ref={forwardedRef}
-    >
-      {children}
-    </Accordion.Item>
-  )
-);
-
-const AccordionTrigger = React.forwardRef(
-  ({ children, className, ...props }, forwardedRef) => (
-    <Accordion.Header className="flex">
-      <Accordion.Trigger
-        className={classNames(
-          'text-violet11 shadow-mauve6 hover:bg-mauve2 group flex h-[45px] flex-1 cursor-default items-center justify-between bg-white px-5 text-[15px] leading-none shadow-[0_1px_0] outline-none',
-          className
-        )}
-        {...props}
-        ref={forwardedRef}
-      >
-        {children}
-        <RxChevronDown
-          className="text-violet10 ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180"
-          aria-hidden
-        />
-      </Accordion.Trigger>
-    </Accordion.Header>
-  )
-);
-
-const AccordionContent = React.forwardRef(
-  ({ children, className, ...props }, forwardedRef) => (
-    <Accordion.Content
-      className={classNames(
-        'text-mauve11 bg-mauve2 data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp overflow-hidden text-[15px]',
-        className
-      )}
-      {...props}
-      ref={forwardedRef}
-    >
-      {children}
-      {/* <div className="py-[15px] px-5">
-        {children}
-        </div> */}
-    </Accordion.Content>
-  )
-);
 
 export default FLAccordion;
---
+++ packages/components/src/lib/primitives/FLMultiAccordion/index.tsx
@@ -0,0 +1,14 @@
+import styles from './index.module.css';
+
+/* eslint-disable-next-line */
+export interface IndexProps {}
+
+export function Index(props: IndexProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Index!</h1>
+    </div>
+  );
+}
+
+export default Index;
